1cfe2a6f70958a302dbb2b443fc6a76a77636729
vinicfrancisco <vini.ibi@gmail.com>
2020-03-25 15:12:21 -0300

    added login method

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useCallback } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+import api from '../../services/api';
 
 import heroesImage from '../../assets/heroes.png';
 import logoImage from '../../assets/logo.svg';
@@ -8,15 +9,40 @@ import logoImage from '../../assets/logo.svg';
 import './styles.css';
 
 export default function Logon() {
+  const history = useHistory();
+
+  const [id, setId] = useState('');
+
+  const handleLogon = useCallback(async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+
+      const { name } = response.data;
+
+      localStorage.setItem('id', id);
+      localStorage.setItem('ongName', name);
+
+      history.push('/profile');
+    } catch (error) {
+      alert('Falha ao realizar logon');
+    }
+  }, [id]);
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImage} alt="Be The Heroe" className="logo" />
 
-        <form>
+        <form onSubmit={handleLogon}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
 
           <button className="button" type="submit">Entrar</button>
 
